bed38cba32ce5c35bec2c6d377b9ad59c3ad6ce8
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2024-07-17 13:13:00 +0200

    update build conf for dormammu

--- build_phase_diagram.py
+++ build_phase_diagram.py
@@ -6,12 +6,12 @@ import numpy as np
 import json
 
 #N = 3
-pool_size = 20
-num_drones = 5
-duration = 200
+pool_size = 40
+num_drones = 10
+duration = 150
 samples = 10
 speed = 1.
-log_file_path = 'test_phase/'
+log_file_path = '/media/Data4/hattenberger_data/test_phase_3/'
 
 
 p = Pool(pool_size)
@@ -21,8 +21,8 @@ config = {
         'duration': duration,
         'samples': samples,
         'speed': speed,
-        'y_att': [0., 1.0, 0.05],
-        'y_ali': [0., 0.4, 0.05]
+        'y_att': [0., 2.0, 0.05],
+        'y_ali': [0., 1.0, 0.05]
         }
 
 config_file = os.path.join(log_file_path, 'config.json')
@@ -31,27 +31,33 @@ with open(config_file, 'w') as f:
 
 def run_cmd(param: dict):
     cmd = ['python', 'simu_phase_diagram.py',
-            '--swarm_config=../UavSwarmFish/config/demo.yaml',
+            '--swarm_config=../UavSwarmFish/config/phase_diagram.yaml',
             f'--num_drones={num_drones}', f'--duration_sec={duration}',
             f'--log_name={param["name"]}',
             f'--log_file_path={log_file_path}',
             f'--y_att={param["y_att"]}',
-            f'--y_ali={param["y_ali"]}'
+            f'--y_ali={param["y_ali"]}',
+            '--save_quant=True'
             ]
     subprocess.run(cmd)
-    print("Done:",cmd)
+    print(f'Done [{param["count"]}]: {' '.join(cmd)}')
 
 y_atts = np.arange(config['y_att'][0], config['y_att'][1], config['y_att'][2])
 y_alis = np.arange(config['y_ali'][0], config['y_ali'][1], config['y_ali'][2])
 
+nb_run = len(y_atts)*len(y_alis)*samples
+print(f'Starting {nb_run} simulations')
+idx = 0
 params = []
 for i, y_att in enumerate(y_atts):
     for j, y_ali in enumerate(y_alis):
         for k in range(samples):
+            idx += 1
             param = {
                     'name': f'run__{i}_{j}_{k}',
                     'y_att': y_att,
                     'y_ali': y_ali,
+                    'count': f'{idx} / {nb_run}'
                     }
             params.append(param)
 
